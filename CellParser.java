774c34091ac7d9603ee8899cc11b40087bcf13fc
Tim Coutinho <tmcoutinho42@gmail.com>
2017-05-06 16:36:21 -0700

    Added package

--- CellParser.java
+++ CellParser.java
@@ -1,10 +1,11 @@
+package TextExcel;
 
 public class CellParser {
 	
 	public static Cell parseCell(String input) throws Exception {
 		if (input.contains("\"")) {
 			return new StringCell(input.replace("\"", ""));
-		} else if(input.contains("(") && input.contains(")")) {
+		} else if (input.contains("(") && input.contains(")")) {
 			return new FormulaCell(input);
 		} else if (input.contains("/")) {
 			return new DateCell(input);
