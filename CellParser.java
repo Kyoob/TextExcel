d7e1ed1afc399cd5a3f995906e5f5fe3c84e1888
Tim Coutinho <tmcoutinho42@gmail.com>
2017-05-07 01:57:41 -0700

    Add files via upload

--- CellParser.java
+++ CellParser.java
@@ -3,9 +3,10 @@ package TextExcel;
 public class CellParser {
 	
 	public static Cell parseCell(String input) throws Exception {
-		return (input.contains("\"")) ? new StringCell(input.replace("\"", "")) : (input.contains("(") && input.contains(")")
-									  ? new FormulaCell(input) : (input.contains("/")
-									  ? new DateCell(input) : new DoubleCell(Double.parseDouble(input))));
+		return (input.contains("\""))					   ? new StringCell(input.replace("\"", ""))
+			 : (input.contains("(") && input.contains(")") ? new FormulaCell(input)
+			 : (input.contains("/")						   ? new DateCell(input)
+			 :												 new DoubleCell(Double.parseDouble(input))));
 	}
 	
 }
