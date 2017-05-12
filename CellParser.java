fb15e59b5bc6ff5ddfb89a3c76300c15dc5b8109
Tim Coutinho <tmcoutinho42@gmail.com>
2017-05-12 01:43:08 -0700

    parseCell now handles originalValue
    Used to be setValue in CellMatrix

--- CellParser.java
+++ CellParser.java
@@ -1,11 +1,11 @@
 
 public class CellParser {
 	
-	public static Cell parseCell(String input) throws Exception {
-		return (input.contains("\""))					   ? new StringCell(input.replace("\"", ""))
-			 : (input.contains("(") && input.contains(")") ? new FormulaCell(input)
-			 : (input.contains("/")						   ? new DateCell(input)
-			 :												 new DoubleCell(Double.parseDouble(input))));
+	public static Cell parseCell(String input, String original) throws Exception {
+		return (input.contains("\""))					   ? new StringCell(input.replace("\"", ""), original)
+			 : (input.contains("(") && input.contains(")") ? new FormulaCell(input, original)
+			 : (input.contains("/")						   ? new DateCell(input, original)
+			 :												 new DoubleCell(Double.parseDouble(input), original)));
 	}
 	
 }
