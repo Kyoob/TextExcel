7f0a1d0d55f89b5fc76aa5683a999f258af258c2
Tim Coutinho <tmcoutinho42@gmail.com>
2017-06-03 16:18:22 -0700

    Renamed cellmatrix variable to instance

--- TextExcel.java
+++ TextExcel.java
@@ -1,6 +1,8 @@
 
 import java.util.Scanner;
 
+// A simple spreadsheet program.
+
 public class TextExcel {
 	
 	public static final int ROWS = 11;
@@ -9,22 +11,22 @@ public class TextExcel {
 	
 	public static void main(String[] args) throws Exception {
 		
-		CellMatrix cellmatrix = CellMatrix.getInstance();
+		CellMatrix instance = CellMatrix.getInstance();
 		System.out.print("Welcome to TextExcel!\n\nEnter a command: ");
 		Scanner console = new Scanner(System.in);
 		String rawInput = console.nextLine();
 		while (!(rawInput.equalsIgnoreCase("exit") || rawInput.equalsIgnoreCase("quit"))) {
 			if (rawInput.contains("clear"))
-				cellmatrix.clear(rawInput);
+				instance.clear(rawInput);
 			else if (rawInput.equalsIgnoreCase("print"))
-				cellmatrix.print();
+				instance.print();
 			else if (rawInput.contains("=")) {
 				int[] cell = findCoords(rawInput.substring(0, 2));
 				String inputValue = rawInput.substring(rawInput.indexOf("=") + 2);
-				cellmatrix.setValue(cell[0], cell[1], inputValue, rawInput);
+				instance.setValue(cell[0], cell[1], inputValue, rawInput);
 			} else {
-				Cell c = cellmatrix.getCell(rawInput);
-				String inputValue = cellmatrix.getValue(c);
+				Cell c = instance.getCell(rawInput);
+				String inputValue = instance.getValue(c);
 				if (inputValue == null)
 					System.out.println(rawInput + " = <empty>");
 				else
@@ -41,13 +43,12 @@ public class TextExcel {
 	public static int[] findCoords(String s) {
 		int col = s.charAt(0) - 'A';
 		int row = Integer.parseInt(s.charAt(1) + "") - 1;
-		if (col >= 0 && col < COLUMNS) {
-			if (!(row < 0 || row > ROWS)) {
-				int result[] = {row + 1, col + 1};
-				return result;
-			} else
+		if (col >= 0 && col < COLUMNS)
+			if (!(row < 0 || row > ROWS))
+				return new int[] {row + 1, col + 1};
+			else
 				System.err.println("Error: Row out of bounds or invalid input.");
-		} else
+		else
 			System.err.println("Error: Column out of bounds or invalid input.");
 		return null;
 	}
