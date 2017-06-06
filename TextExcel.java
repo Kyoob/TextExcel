fa96bd2c1f98259ae0941e743895b83bcafde6a0
Tim Coutinho <tmcoutinho42@gmail.com>
2017-06-05 23:57:34 -0700

    Made user input more workable

--- TextExcel.java
+++ TextExcel.java
@@ -22,9 +22,9 @@ public class TextExcel {
 			else if (rawInput.equalsIgnoreCase("print") || rawInput.equalsIgnoreCase("p"))
 				instance.print();
 			else if (rawInput.contains("=")) {
-				int[] cell = findCoords(rawInput.substring(0, rawInput.indexOf('=') - 1));
-				String inputValue = rawInput.substring(rawInput.indexOf("=") + 2);
-				instance.setValue(cell[0], cell[1], inputValue, rawInput);
+				int[] cell = findCoords(rawInput.substring(0, rawInput.replace(" ", "").indexOf('=')));
+				if (cell != null)
+					instance.setValue(cell[0], cell[1], rawInput.substring(rawInput.indexOf("=") + 1), rawInput);
 			} else
 				try {
 					Cell c = instance.getCell(rawInput);
@@ -45,12 +45,12 @@ public class TextExcel {
 		int col = cellString.charAt(0) - 'A';
 		int row = Integer.parseInt(cellString.substring(1)) - 1;
 		if (col >= 0 && col < COLUMNS)
-			if (!(row < 0 || row > ROWS))
+			if (row >= 0 && row < ROWS - 1)
 				return new int[] {row + 1, col + 1};
 			else
-				System.err.println("Error: Row out of bounds or invalid input.");
+				System.err.println("Error: Row out of bounds.");
 		else
-			System.err.println("Error: Column out of bounds or invalid input.");
+			System.err.println("Error: Column out of bounds.");
 		return null;
 	}
 	
